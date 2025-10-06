4852fc8ef66c29543aefc960fe5be62e3c41dbdb
MihaiCatalin120 <mihaicatalin449@gmail.com>
2025-10-06 18:05:58 +0300

    Fight bar: Add placeholder image

---
+++ src/lib/components/FightBar.svelte
@@ -0,0 +1,4 @@
+<div class="flex h-1/4 w-full justify-between rounded-md border-white bg-black px-2 text-white">
+	<img src="/images/bomboana.png" width="150" alt="enemy" />
+	<img src="/images/bomboana.png" width="150" alt="enemy" />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import GameGrid from '$lib/components/GameGrid.svelte';
 	import TooltipBar from '$lib/components/TooltipBar.svelte';
+	import FightBar from '$lib/components/FightBar.svelte';
 </script>
 
 <div class="flex h-full w-full flex-col gap-2 p-3">
-	<div class="h-1/4 w-full rounded-md border-white bg-black"></div>
+	<FightBar />
 	<div class="flex h-3/4 w-full rounded-md border-black bg-white">
 		<GameGrid />
 		<TooltipBar />
